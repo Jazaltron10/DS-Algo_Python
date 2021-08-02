6aca0bda24ac57d736fc974b3b3e98ee6815d543
Jazaltron <jazaltron10@gmail.com>
2021-08-02 08:11:52 +0100

    Modified the classes file: OOP On the Way

---
+++ FreeStyles/Freestyle_11-Classes.py
@@ -0,0 +1,10 @@
+message = "Welcome to Classes"
+print("\n" + message)
+message.upper()
+print("\n" + message.upper() )
+
+
+while True:
+    if message.startswith("W"):
+        print("\n" + message)
+    break;
